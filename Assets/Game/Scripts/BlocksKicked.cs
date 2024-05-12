7913f561ef7256b2622da9c833a3929ce4714bb4
bennieb1 <brandon961@live.ca>
2024-05-11 21:28:16 -0400

    Traps
    walks in to blocks and makes noise as well as a crouch with possibly better CC

---
+++ Assets/Game/Scripts/BlocksKicked.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlocksKicked : MonoBehaviour
+{
+
+    public Rigidbody blockRigidbody;
+    public AudioSource audio;
+    public Animator anim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Block")) // Make sure your blocks have the tag "Block"
+        {
+             blockRigidbody = other.GetComponent<Rigidbody>();
+            if (blockRigidbody != null)
+            {
+                // Apply force to simulate the kick
+                blockRigidbody.AddForce(transform.forward * 500); // Adjust force as necessary
+
+
+
+
+                 audio = other.GetComponent<AudioSource>();
+                if (audio != null)
+                {
+                    audio.Play();
+                    anim = other.GetComponent<Animator>();
+                
+                    
+                        anim.SetTrigger("Kick");
+                  
+
+                }
+            }
+        }
+    }
+
+
+
+
+
+}
