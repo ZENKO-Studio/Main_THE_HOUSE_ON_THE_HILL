0ef9f9720fe1bbbd09f58d02f08339ae2300731a
bennieb1 <brandon961@live.ca>
2024-05-12 13:21:32 -0400

    Revert "Reapply "Merge branch 'Developing(90%)' into BrandonUpdateController""
    This reverts commit 9c7104ea5f41b247687c9b3094bc4a0c487ddee8.

--- Assets/Game/Scripts/BlocksKicked.cs
+++ Assets/Game/Scripts/BlocksKicked.cs
@@ -8,7 +8,6 @@ public class BlocksKicked : MonoBehaviour
     public Rigidbody blockRigidbody;
     public AudioSource audio;
 
-    public Animator anim;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -27,12 +26,7 @@ public class BlocksKicked : MonoBehaviour
                 if (audio != null)
                 {
                     audio.Play();
-
-                    anim = other.GetComponent<Animator>();
-                
-                    
-                        anim.SetTrigger("Kick");
-
+               
                   
 
                 }
