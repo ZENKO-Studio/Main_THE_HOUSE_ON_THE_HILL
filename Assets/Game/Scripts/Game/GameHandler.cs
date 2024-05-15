c0ff50b71d358fbb49c89c7e77ec2b428f22b3d1
sami-s-56 <samisyed301@gmail.com>
2024-05-15 15:08:27 -0400

    Hooked up HUD to Player

--- Assets/Game/Scripts/Game/GameHandler.cs
+++ Assets/Game/Scripts/Game/GameHandler.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameHandler : MonoBehaviour
+public class GameHandler : Singleton<GameHandler>
 {
+    //Should be set on game start or manually in the scene
+    public PlayerCharacter playerRef;
+
     // Start is called before the first frame update
     void Start()
     {
