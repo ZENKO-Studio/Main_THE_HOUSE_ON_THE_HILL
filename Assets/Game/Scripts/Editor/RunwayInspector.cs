431cace04641e572ac5f4c1b75ab00bdc65ab9c5
sami-s-56 <samisyed301@gmail.com>
2024-05-16 15:12:41 -0400

    Playground Stuff partial

---
+++ Assets/Game/Scripts/Editor/RunwayInspector.cs
@@ -0,0 +1,15 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Runway))]
+public class RunwayInspector : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        
+
+        // Add a custom button
+        
+    }
+}
